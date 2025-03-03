55252f5304532edfd507bfba8f49e76d010a807f
shree256 <shreeshan256@gmail.com>
2025-03-03 22:00:25 +0530

    Updates send_logs_to_clickhouse

--- easyaudit/tasks.py
+++ easyaudit/tasks.py
@@ -1,7 +1,8 @@
 import logging
 import clickhouse_connect
 
-from celery import shared_task
+from datetime import timedelta
+from django.utils.timezone import now
 
 from .models import CRUDEvent, LoginEvent
 from .serializers import CRUDEventSerializer, LoginEventSerializer
@@ -15,25 +16,21 @@ from .settings import (
 logger = logging.getLogger(__name__)
 
 
-@shared_task
 def send_logs_to_clickhouse():
     logger.info("Clickhouse: Initiating data transfer...")
 
-    # CRUD Event logs
-    crud_logs = CRUDEvent.objects.all()
-    crud_serializer = CRUDEventSerializer(crud_logs, many=True)
-    crud_row_matrix = []
+    # Get the timestamp for 24 hours ago
+    time_threshold = now() - timedelta(hours=24)
 
-    for log in crud_serializer.data:
-        crud_row_matrix.append(list(log.values()))
+    # CRUD Event logs
+    crud_logs = CRUDEvent.objects.filter(created_at__gte=time_threshold)
+    crud_serializer = CRUDEventSerializer(crud_logs, many=True)
+    crud_row_matrix = [list(log.values()) for log in crud_serializer.data]
 
     # Login Event logs
-    login_logs = LoginEvent.objects.all()
+    login_logs = LoginEvent.objects.filter(created_at__gte=time_threshold)
     login_serializer = LoginEventSerializer(login_logs, many=True)
-    login_row_matrix = []
-
-    for log in login_serializer.data:
-        login_row_matrix.append(list(log.values()))
+    login_row_matrix = [list(log.values()) for log in login_serializer.data]
 
     logger.info(
         "Clickhouse: Collected %s crud logs and %s login logs",
