bdd529b896f90e87c0a0cb1eaf54a6e599062a2a
shree256 <shreeshan256@gmail.com>
2025-03-04 23:25:57 +0530

    Adds new migrations

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-easy-audit-clickhouse",
-    version="1.0.1rc1",
+    version="1.0.4",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
