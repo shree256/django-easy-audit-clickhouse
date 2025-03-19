6fafd1a745d4d9b8d25ba28fd37f369b38aad953
shree256 <shreeshan256@gmail.com>
2025-03-19 23:54:12 +0530

    Adds ExternalServiceLogSerializer

--- easyaudit/serializers.py
+++ easyaudit/serializers.py
@@ -23,3 +23,17 @@ class LoginEventSerializer(serializers.Serializer):
 
     def get_created_at(self, obj):
         return obj.created_at
+
+
+class ExternalServiceLogSerializer(serializers.Serializer):
+    service_name = serializers.CharField(read_only=True, default="")
+    protocol = serializers.CharField(read_only=True, default="")
+    request_repr = serializers.CharField(read_only=True, default="")
+    response_repr = serializers.CharField(read_only=True, default="")
+    error_message = serializers.CharField(read_only=True, default="")
+    execution_time = serializers.FloatField(read_only=True, default=0)
+    created_at = serializers.SerializerMethodField(read_only=True)
+    user_id = serializers.CharField(read_only=True, default="")
+
+    def get_created_at(self, obj):
+        return obj.created_at
